144c750029722e561a7bd2fc1519144b75f4d3f1
Caleb Bellmyer <cjbellmyer5984@gmail.com>
2026-05-08 20:55:25 -0500

    products centered on home

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,8 @@ import {
 } from "../components/ProductGrid";
 
 // TODO: replace with the product type string from your Shopify admin.
+//  map cases
+const PRODUCT_TYPE = "case";
 
 export const Route = createFileRoute("/")({
   loader: async ({ context: { queryClient } }) => {
@@ -21,7 +23,8 @@ export const Route = createFileRoute("/")({
 
 function App() {
   return (
-    <main className="page-wrap px-4 pb-8 pt-14">
+    <main className="page-wrap px-4 pb-8 pt-14 flex flex-col items-center">
+      {/*The following divs should be horizontally centered*/}
       <div>
         <h2 className="text-2xl font-bold mb-4">Cases</h2>
         <ProductGrid productType={"case"} />
